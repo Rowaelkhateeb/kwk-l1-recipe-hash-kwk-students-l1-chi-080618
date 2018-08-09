4c5528347c6eef051b4a46e2fd64976197f2f697
Rowaelkhateeb <rowaelkhateeb2001@gmail.com>
2018-08-09 15:36:57 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -28,6 +28,7 @@ end
 def ingredients_and_amounts(perfect_10_recipe)
   puts perfect_10_recipe.each do |x,y|
     "#{keys} and #{values}"
+  end
 end
 
 # 4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
@@ -41,4 +42,4 @@ end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
-end# 
+end
