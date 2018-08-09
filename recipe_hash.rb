49c1b62bc8c9db7093419ac054d1af24d4b94512
Rowaelkhateeb <rowaelkhateeb2001@gmail.com>
2018-08-09 15:47:40 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -47,5 +47,6 @@ end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
-  puts perfect_10_recipe.length
+  perfect_10_recipe.length
 end
+  puts number_ingredients(perfect_10_recipe)
